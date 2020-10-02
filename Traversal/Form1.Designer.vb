873ae46c2c716a13185745f4ef0fddf71ee170ee
glitchwizard <glitchwizard@gmail.com>
2020-10-02 14:42:01 -0600

    Initial commit, working on traversing subfolders

---
+++ Traversal/Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TraversalForm
+	Inherits System.Windows.Forms.Form
+
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
+
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()> _
+	Private Sub InitializeComponent()
+        Me.TraversFilesButton = New System.Windows.Forms.Button()
+        Me.VaultsComboBox = New System.Windows.Forms.ComboBox()
+        Me.VaultsLabel = New System.Windows.Forms.Label()
+        Me.TraverseFoldersButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TraversFilesButton
+        '
+        Me.TraversFilesButton.Location = New System.Drawing.Point(34, 72)
+        Me.TraversFilesButton.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TraversFilesButton.Name = "TraversFilesButton"
+        Me.TraversFilesButton.Size = New System.Drawing.Size(101, 41)
+        Me.TraversFilesButton.TabIndex = 18
+        Me.TraversFilesButton.Text = "Traverse Files"
+        Me.TraversFilesButton.UseVisualStyleBackColor = True
+        '
+        'VaultsComboBox
+        '
+        Me.VaultsComboBox.FormattingEnabled = True
+        Me.VaultsComboBox.Location = New System.Drawing.Point(15, 36)
+        Me.VaultsComboBox.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.VaultsComboBox.Name = "VaultsComboBox"
+        Me.VaultsComboBox.Size = New System.Drawing.Size(136, 21)
+        Me.VaultsComboBox.TabIndex = 16
+        '
+        'VaultsLabel
+        '
+        Me.VaultsLabel.AutoSize = True
+        Me.VaultsLabel.Location = New System.Drawing.Point(15, 20)
+        Me.VaultsLabel.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.VaultsLabel.Name = "VaultsLabel"
+        Me.VaultsLabel.Size = New System.Drawing.Size(136, 13)
+        Me.VaultsLabel.TabIndex = 17
+        Me.VaultsLabel.Text = "Select Preferred Vault View"
+        '
+        'TraverseFoldersButton
+        '
+        Me.TraverseFoldersButton.Location = New System.Drawing.Point(25, 131)
+        Me.TraverseFoldersButton.Name = "TraverseFoldersButton"
+        Me.TraverseFoldersButton.Size = New System.Drawing.Size(117, 21)
+        Me.TraverseFoldersButton.TabIndex = 19
+        Me.TraverseFoldersButton.Text = "Traverse Folders"
+        Me.TraverseFoldersButton.UseVisualStyleBackColor = True
+        '
+        'TraversalForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(176, 164)
+        Me.Controls.Add(Me.TraverseFoldersButton)
+        Me.Controls.Add(Me.TraversFilesButton)
+        Me.Controls.Add(Me.VaultsComboBox)
+        Me.Controls.Add(Me.VaultsLabel)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Name = "TraversalForm"
+        Me.Text = "Traversal"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TraversFilesButton As System.Windows.Forms.Button
+	Friend WithEvents VaultsComboBox As System.Windows.Forms.ComboBox
+	Friend WithEvents VaultsLabel As System.Windows.Forms.Label
+	Friend WithEvents TraverseFoldersButton As Button
+End Class
---
+++ Traversal/Form1.vb
@@ -0,0 +1,112 @@
+﻿Imports EPDM.Interop.epdm
+
+Public Class TraversalForm
+	Private Sub TraversalForm_Load(ByVal sender As System. _
+		Object, ByVal e As System.EventArgs) Handles MyBase. _
+		Load
+
+		Dim vault As IEdmVault8 = New EdmVault5
+		Dim Views() As EdmViewInfo = {}
+
+		vault.GetVaultViews(Views, False)
+		VaultsComboBox.Items.Clear()
+		For Each View As EdmViewInfo In Views
+			VaultsComboBox.Items.Add(View.mbsVaultName)
+		Next
+		If VaultsComboBox.Items.Count > 0 Then
+			VaultsComboBox.Text = VaultsComboBox.Items(0)
+		End If
+	End Sub
+
+	Private Sub TraversFilesButton_Click(ByVal sender As _
+		System.Object, ByVal e As System.EventArgs) Handles _
+		TraversFilesButton.Click
+
+		'Declare and create an instance of IEdmVault5 object
+		Dim vault As IEdmVault5 = New EdmVault5()
+		'Log into selected vault as the current user
+		vault.LoginAuto(VaultsComboBox.Text, Me.Handle.
+			ToInt32())
+
+		'Get the IEdmFolder5 object of the root folder
+		Dim RootFolder As IEdmFolder5
+		RootFolder = vault.RootFolder
+
+		'Get the path of the root folder
+		Dim FolderPath As String
+		FolderPath = RootFolder.LocalPath
+
+		'Get the name of the root folder from the IEdmObject interface
+		Dim FolderName As String
+		FolderName = RootFolder.Name
+
+		'Get the IEdmPos object for the first file in the folder
+		Dim FilePos As IEdmPos5
+		FilePos = RootFolder.GetFirstFilePosition
+
+		'Enumerate the files in the folder
+		Dim file As IEdmFile5
+		While Not FilePos.IsNull
+			file = RootFolder.GetNextFile(FilePos)
+			'Get the name of each file from the IEdmOjbect interface
+			Dim FileName As String = file.Name
+			'Get its checked out status
+			Dim CheckedOutStatus As String
+			CheckedOutStatus = IIf(file.IsLocked, "", " not")
+			Debug.Print(" " + FileName + " -" + CheckedOutStatus + " checked out")
+		End While
+	End Sub
+
+	Private Sub TraverseFoldersButton_Click(sender As Object, e As EventArgs) Handles TraverseFoldersButton.Click
+		'Declare and create an instance of IEdmVault5 object
+		Dim vault As IEdmVault5 = New EdmVault5()
+		'Log into selected vault as the current user
+		vault.LoginAuto(VaultsComboBox.Text, Me.Handle.
+			ToInt32())
+
+		'Get the IEdmFolder5 object of the root folder
+		Dim RootFolder As IEdmFolder5
+		RootFolder = vault.RootFolder
+
+		'Get the path of the root folder
+		Dim FolderPath As String
+		FolderPath = RootFolder.LocalPath
+
+		'Get the name of the root folder from the IEdmObject interface
+		Dim FolderName As String
+		FolderName = RootFolder.Name
+
+		'Get the IEdmPos object for the first file in the folder
+		Dim FilePos As IEdmPos5
+		FilePos = RootFolder.GetFirstFilePosition
+
+		'Enumerate the files in the folder
+		Dim file As IEdmFile5
+		While Not FilePos.IsNull
+			file = RootFolder.GetNextFile(FilePos)
+			'Get the name of each file from the IEdmOjbect interface
+			Dim FileName As String = file.Name
+			'Get its checked out status
+			Dim CheckedOutStatus As String
+			CheckedOutStatus = IIf(file.IsLocked, "", " not")
+			Debug.Print(" " + FileName + " -" + CheckedOutStatus + " checked out")
+		End While
+
+		'Enumerate the fub-folders in the root folder
+		Dim FolderPos As IEdmPos5
+		FolderPos = RootFolder.GetFirstSubFolderPosition
+		While Not FolderPos.IsNull
+			Dim SubFolder As IEdmFolder5
+			SubFolder = RootFolder.GetNextSubFolder(FolderPos)
+			Debug.Print("+" + SubFolder.Name)
+		End While
+	End Sub
+	Private Sub TraverseFolder(ByVal CurrentFolder As IEdmFolder5, ByVal Level As Integer)
+
+		Dim FolderName As String = CurrentFolder.Name
+		Dim Indent As String = New String(" ", Level * 2)
+
+		Debug.Print(Indent + "+" + FolderName)
+
+	End Sub
+End Class
